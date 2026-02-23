80e0563869367a67c9ded3145ddfb5a4d01b0eba
unknown <sophiathanhmai@gmail.com>
2026-02-23 09:55:08 -0500

    todos-02: Configure Tailwind CSS and Prettier

---
+++ src/main.js
@@ -0,0 +1,4 @@
+import "./style.css";
+
+const app = document.querySelector("#app");
+app.innerHTML = `<div class="flex justify-center border border-red-500 h-screen items-center" >Hello World!</div>`;
