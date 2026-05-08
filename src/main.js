143936ec1cf065a8b8883cbf8f84a9ae21c5eeb1
unknown <sophiathanhmai@gmail.com>
2026-05-08 17:38:22 -0400

    todos-03: Configure GitHub Pages deployment

--- src/main.js
+++ src/main.js
@@ -1,6 +1,4 @@
 import "./style.css";
 
 const app = document.getElementById("app");
-app.innerHTML = `<div class="flex items-center justify-center h-screen">
-  <div>Hello World!</div>
-</div>`;
+app.innerHTML = `<div>Hello World!</div>`;
