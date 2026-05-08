98ffe7bfdf644abac4cf278d608d59be47e55488
unknown <sophiathanhmai@gmail.com>
2026-05-08 18:22:23 -0400

    todos-08: Implement todo filtering

--- src/main.js
+++ src/main.js
@@ -15,8 +15,20 @@ function renderTodos() {
   const todoListElement = document.getElementById("todo-list");
   todoListElement.innerHTML = "";
 
+  const filteredTodos = [];
   for (let i = 0; i < todos.length; i++) {
     const todo = todos[i];
+    if (filter === "all") {
+      filteredTodos.push(todo);
+    } else if (filter === "completed" && todo.completed) {
+      filteredTodos.push(todo);
+    } else if (filter === "active" && !todo.completed) {
+      filteredTodos.push(todo);
+    }
+  }
+
+  for (let i = 0; i < filteredTodos.length; i++) {
+    const todo = filteredTodos[i];
     const todoItem = document.createElement("div");
     todoItem.classList.add("p-4", "todo-item");
     todoListElement.appendChild(todoItem);
@@ -34,13 +46,21 @@ function renderTodos() {
     todoEdit.value = todo.text;
     todoItem.appendChild(todoEdit);
   }
+
+  const activeHref =
+    filter === "active"
+      ? todoNav.children[1].href
+      : filter === "completed"
+        ? todoNav.children[2].href
+        : todoNav.children[0].href;
+  renderTodoNavBar(activeHref);
 }
 
 function handleNewTodoKeyDown(event) {
   const newTodoInput = event.target;
   const todoText = newTodoInput.value.trim();
   if (event.key === "Enter" && todoText !== "") {
-    todo.push({ id: nextTodoId++, text: todoText, completed: false });
+    todos.push({ id: nextTodoId++, text: todoText, completed: false });
     newTodoInput.value = "";
     renderTodos();
   }
@@ -48,3 +68,38 @@ function handleNewTodoKeyDown(event) {
 
 const newTodoInput = document.getElementById("new-todo");
 newTodoInput.addEventListener("keydown", handleNewTodoKeyDown);
+
+function renderTodoNavBar(href) {
+  const elements = todoNav.children;
+  for (let i = 0; i < elements.length; i++) {
+    const element = elements[i];
+    if (element.href === href) {
+      element.classList.add(
+        "underline",
+        "underline-offset-4",
+        "decoration-rose-800",
+        "decoration-2",
+      );
+    } else {
+      element.classList.remove(
+        "underline",
+        "underline-offset-4",
+        "decoration-rose-800",
+        "decoration-2",
+      );
+    }
+  }
+}
+
+function handleClickOnNavbar(event) {
+  if (event.target.tagName === "A") {
+    const hrefValue = event.target.href;
+    const action = hrefValue.split("/").pop();
+    filter = action === "" ? "all" : action;
+    renderTodos();
+    renderTodoNavBar(hrefValue);
+  }
+}
+
+const todoNav = document.getElementById("todo-nav");
+todoNav.addEventListener("click", handleClickOnNavbar);
